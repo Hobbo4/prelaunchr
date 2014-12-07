09328e63e053156de388f68504e1e717ce3c4116
Adam Hobson <adamlhobson@gmail.com>
2014-12-07 22:53:00 +0000

    Zapier added zapier

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+		establish_connection :adapter => "postgresql", :database => "zapier"
     belongs_to :referrer, :class_name => "User", :foreign_key => "referrer_id"
     has_many :referrals, :class_name => "User", :foreign_key => "referrer_id"
     
