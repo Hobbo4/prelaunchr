6ed85c0fbd6be370977407d6d29b76d1b185a20d
Adam Hobson <adamlhobson@gmail.com>
2014-12-07 20:52:44 +0000

    added zapier db2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 
     before_create :create_referral_code
     after_create :send_welcome_email
+		establish_connection :zapier_development
 	
 
     REFERRAL_STEPS = [
