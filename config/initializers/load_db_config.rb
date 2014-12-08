e44d7633f7ad58c0acb4c976de4a69a92c4dd731
Adam Hobson <adamlhobson@gmail.com>
2014-12-08 12:28:33 +0000

    update

--- config/initializers/load_db_config.rb
+++ config/initializers/load_db_config.rb
@@ -1,5 +1,4 @@
 PRIMARY_DB_CONFIG = YAML.load_file("#{Rails.root}/config/db.yml")[Rails.env]
 
 # As our secondary db is being defined in production_sec.
-SECONDARY_DB_CONFIG = YAML.load_file("#{Rails.root}/config/db.yml")[Rails.env+
-                                                                        '_sec']
+SECONDARY_DB_CONFIG = YAML.load_file("#{Rails.root}/config/db.yml")[Rails.env+'_sec']
